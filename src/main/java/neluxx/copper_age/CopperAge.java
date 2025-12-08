1e270af7ddcd71fb26697266e145a4634bdead08
Neluxx <fabian.arndt96@proton.me>
2025-12-08 21:06:07 +0100

    refactor: move datapack and resourcepack into resourcepacks directory

--- src/main/java/neluxx/copper_age/CopperAge.java
+++ src/main/java/neluxx/copper_age/CopperAge.java
@@ -6,39 +6,23 @@ import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class CopperAge implements ModInitializer {
 	public static final String MOD_ID = "copper-age";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-
         // Register the datapack
         ResourceManagerHelper.registerBuiltinResourcePack(
-                Identifier.of("copper_age", "datapack"),
+                Identifier.of(MOD_ID, "datapack"),
                 FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow(),
                 ResourcePackActivationType.ALWAYS_ENABLED
         );
 
         // Register the resource pack
         ResourceManagerHelper.registerBuiltinResourcePack(
-                Identifier.of("copper_age", "resourcepack"),
+                Identifier.of(MOD_ID, "resourcepack"),
                 FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow(),
                 ResourcePackActivationType.DEFAULT_ENABLED
         );
-
-        LOGGER.info("Copper Age datapack and resourcepack registered!");
 	}
 }
